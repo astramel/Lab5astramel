66f2f6684f1e0cbb5f50ecbbed2f31bbfa73abfc
astramel <35742023+astramel@users.noreply.github.com>
2018-03-12 20:22:17 -0600

    Update Lab5MyStack.h

--- Lab5MyStack.h
+++ Lab5MyStack.h
@@ -1,5 +1,13 @@
-//MyStack function declarations
-#include<string>
+//Sasha Stramel
+
+#pragma once
+#ifndef LAB5MYSTACK_H
+#define LAB5MYSTACK_H
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
 
 std::string stringReversal1(std::string input);
 std::string stringReversal2(std::string input);
@@ -7,6 +15,9 @@ std::string stringReversal3(std::string input);
 std::string stringReversal4(std::string input);
 
 class MyStack{
+  plubic:
+  vector<char> stack;
+ // list<char> stack;
   bool isEmpty() const;
   void push(char& c);
   char pull();
